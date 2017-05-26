18b3db7b4c24541651b4bdaff2796d3cd9ddd83f
paul <paul@paul-server>
2017-05-26 16:35:39 +0200

    Change LOGIN_HASH_LENGTH constant to 128, in order to match the database

--- src/login.h
+++ src/login.h
@@ -5,7 +5,7 @@
 	#define _kore_login_h_
 
 	#define LOGIN_HASH_ITERATIONS 4096
-	#define LOGIN_HASH_LENGTH 256
+	#define LOGIN_HASH_LENGTH 128
 	void login_hash_password(const char* pass, const unsigned char* salt, int32_t iterations, uint32_t outputBytes, char* hexResult);
 	char * login_generate_salt(int length);
 	bool login_validate_password(char * input_password, char * hash, char * salt); //compares input password to password in database
